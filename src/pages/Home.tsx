6702044b9090a73731b5e49fd9227ffa6f737e5e
yasaman <ysmn.dev@gmail.com>
2024-10-18 14:39:45 +0330

    Import json file and show data on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,35 @@
+import React, { useState, useEffect } from "react";
+import data from "../data/valuable_wallets_sample.json"; // Import mock data
+
+interface Wallet {
+  walletAddress: string;
+  netProfit: number;
+}
+
 const HomePage = () => {
-  return <div className=" "> Home Page</div>;
+  const [wallets, setWallets] = useState<Wallet[]>([]);
+
+  useEffect(() => {
+    if (Array.isArray(data)) {
+      setWallets(data);
+    } else {
+      console.error("Invalid data format", data);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        {wallets.map((wallet, index) => (
+          <li key={index}>
+            <p>Address: {wallet.walletAddress}</p>
+            <p>Net Profit: {wallet.netProfit}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
